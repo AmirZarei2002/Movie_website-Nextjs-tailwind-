24bd70c8e6fba5abeb0c1bd437c45a86762e7d6a
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-02-01 23:13:39 +0330

    image deleted and some animations set

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+// icons
 import {
     AiOutlineHome,
     AiOutlineSearch,
@@ -8,19 +8,27 @@ import { VscAccount } from 'react-icons/vsc';
 import { BsCollection, BsPatchCheck } from 'react-icons/bs';
 import { RiMovie2Line } from 'react-icons/ri';
 
+// dark mode btn
 import DarkModeButton from './DarkModeButton';
 
-import img from '../public/Designbolts-Free-Multimedia-Film.ico';
+// components
 import HeaderItem from './HeaderItem';
+
+// motion
+import { motion } from 'framer-motion';
+import { TypingText } from './CustomTexts';
+import { navVariants } from '../utils';
+
+
 
 function Header() {
     return (
-        <header className="grid grid-cols gap-4 lg:gap-10 sm:flex-row m-4 items-center h-auto">
+        <motion.header variants={navVariants}
+        initial="hidden"
+        whileInView="show" className="grid grid-cols gap-4 lg:gap-10 sm:flex-row m-4 items-center h-auto">
             <div className="flex justify-around lg:justify-between space-x-28 lg:space-x-0">
                 <RiMovie2Line className="text-2xl flex text-white dark:hover:text-cyan-600 w-[2.5rem] h-[2.5rem]" />
-                <h1 className="dark:text-cyan-500 dark:animate-pulse italic hidden sm:block md:text-2xl font-semibold">
-                    Movie Lovers
-                </h1>
+                <TypingText title="Movie Lovers" textStyles="dark:text-cyan-500 dark:animate-pulse italic hidden sm:block md:text-2xl font-semibold"/>
                 <DarkModeButton />
             </div>
             <div className="flex flex-grow justify-between lg:justify-around items-center">
@@ -31,10 +39,8 @@ function Header() {
                 <HeaderItem title="SEARCH" Icon={AiOutlineSearch} />
                 <HeaderItem title="ACCOUNT" Icon={VscAccount} />
             </div>
-            {/* <div>
-               
-            </div> */}
-        </header>
+
+        </motion.header>
     );
 }
 
