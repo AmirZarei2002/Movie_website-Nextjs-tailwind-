76ee6e02ece0f3cd7ebcc90b367d034e91731d9d
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-02-01 23:13:05 +0330

    motion utils added to the project

---
+++ utils/index.js
@@ -0,0 +1,118 @@
+export const navVariants = {
+    hidden: {
+        opacity: 0,
+        y: -50,
+        transition: {
+            type: 'spring',
+            stiffness: 300,
+            damping: 140,
+        },
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: 'spring',
+            stiffness: 80,
+            delay: 0.3,
+        },
+    },
+};
+
+export const slideIn = (direction, type, delay, duration) => ({
+    hidden: {
+        x: direction === 'left' ? '-50%' : direction === 'right' ? '50%' : 0,
+        y: direction === 'up' ? '50%' : direction === 'down' ? '50%' : 0,
+    },
+    show: {
+        x: 0,
+        y: 0,
+        transition: {
+            type,
+            delay,
+            duration: 1,
+            ease: 'easeOut',
+        },
+    },
+});
+
+export const fadeIn = (direction, type, delay, duration) => ({
+    hidden: {
+        x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
+        y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+        opacity: 0,
+    },
+    show: {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        transition: {
+            type,
+            delay,
+            duration,
+            ease: 'easeOut',
+        },
+    },
+});
+
+export const textVariant = (delay) => ({
+    hidden: {
+        y: 50,
+        opacity: 0,
+    },
+    show: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: 'spring',
+            duration: 1.25,
+            delay,
+        },
+    },
+});
+
+export const textContainer = {
+    hidden: {
+        opacity: 0,
+    },
+    show: (i = 1) => ({
+        opacity: 1,
+        transition: { staggerChildren: 0.1, delayChildren: i * 1.5 },
+    }),
+};
+
+export const textVariant2 = {
+    hidden: {
+        opacity: 0,
+        y: 20,
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: 'tween',
+            ease: 'easeIn',
+        },
+    },
+};
+
+export const footerVariants = {
+    hidden: {
+        opacity: 0,
+        y: 50,
+        transition: {
+            type: 'spring',
+            stiffness: 300,
+            damping: 140,
+        },
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: 'spring',
+            stiffness: 80,
+            delay: 0.5,
+        },
+    },
+};
