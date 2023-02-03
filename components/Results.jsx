63e4936338a1bb75acd8146ef18e5fb7d83dbffd
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-02-03 19:11:10 +0330

    new custom texts for this section added

--- components/Results.jsx
+++ components/Results.jsx
@@ -3,7 +3,8 @@ import Thumbnail from './Thumbnail';
 import FlipMove from 'react-flip-move';
 // motion
 import { motion } from 'framer-motion';
-import { fadeIn } from '../utils';
+import { fadeIn, navVariants } from '../utils';
+import { TitleText, TypingText } from './CustomTexts';
 
 const Results = ({ results }) => {
     const [searchTerm, setSearchTerm] = useState('');
@@ -19,18 +20,31 @@ const Results = ({ results }) => {
 
     return (
         <div>
-            <motion.input
-                initial="hidden"
-                whileInView="show"
-                variants={fadeIn('right', 'tween', 0.2, 1)}
-                type="text"
-                placeholder="Search..."
-                value={searchTerm}
-                onChange={(event) => setSearchTerm(event.target.value)}
-                className="w-[20%] px-2 mt-10 focus:outline-none text-black
-         dark:text-white
-         text-[20px] rounded-tr-md rounded-bl-md m-auto block text-center"
-            />
+            <div className="text-center space-y-6 mb-12">
+                <div className="space-y-2">
+                    <TitleText title="Hello and welcome to the Movie Lovers!" />
+                    <motion.div
+                        variants={navVariants}
+                        initial="hidden"
+                        whileInView="show"
+                        className=""
+                    >
+                        <TypingText title="What movie are you looking for?" textStyles="text-xl dark:text-blue-400 purple" />
+                    </motion.div>
+                </div>
+                <motion.input
+                    initial="hidden"
+                    whileInView="show"
+                    variants={fadeIn('right', 'tween', 0.2, 1)}
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                    className="w-[70%] md:w-[50%] lg:w-[40%] px-2 focus:outline-none text-black
+                dark:text-white
+                text-[20px] rounded-tr-md rounded-bl-md m-auto block text-center"
+                />
+            </div>
             <FlipMove
                 className="px-5 mb-10 mt-4 sm:grid md:grid-cols-2 xl:grid-cols-3
     3xl:flex flex-wrap justify-center"
