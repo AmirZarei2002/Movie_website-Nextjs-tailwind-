8c880c4feb21a6c4112dfd341d38f11fe1295986
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-02-06 22:37:43 +0330

    flip move committed to prevent the errors

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import Thumbnail from './Thumbnail';
-import FlipMove from 'react-flip-move';
+// import FlipMove from 'react-flip-move';
+
+
 // motion
 import { motion } from 'framer-motion';
 import { fadeIn, navVariants } from '../utils';
@@ -28,7 +30,10 @@ const Results = ({ results }) => {
                         whileInView="show"
                         className=""
                     >
-                        <TypingText title="What movie are you looking for?" textStyles="text-xl dark:text-blue-400 purple" />
+                        <TypingText
+                            title="What movie are you looking for?"
+                            textStyles="text-xl dark:text-blue-400 purple"
+                        />
                     </motion.div>
                 </div>
                 <motion.input
@@ -44,14 +49,16 @@ const Results = ({ results }) => {
                 text-[20px] rounded-tr-md rounded-bl-md m-auto block text-center"
                 />
             </div>
-            <FlipMove
-                className="px-5 mb-10 mt-4 sm:grid md:grid-cols-2 xl:grid-cols-3
-    3xl:flex flex-wrap justify-center"
-            >
-                {filteredResults.map((result) => (
-                    <Thumbnail key={result.id} result={result} />
-                ))}
-            </FlipMove>
+            <div>
+                <div
+                    className="px-5 mb-10 mt-4 sm:grid md:grid-cols-2 xl:grid-cols-3
+                3xl:flex flex-wrap justify-center"
+                >
+                    {filteredResults.map((result) => (
+                        <Thumbnail key={result.id} result={result} />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
