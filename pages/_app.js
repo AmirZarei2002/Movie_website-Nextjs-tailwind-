412acb5eab548691b619c77a594d0496ef77ac89
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-02-15 23:42:17 +0330

    layout added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css';
+import Layout from '../components/Layout';
 
 import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
     return (
         <ThemeProvider attribute="class">
-            <Component {...pageProps} />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
         </ThemeProvider>
     );
 }
