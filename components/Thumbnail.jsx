588e52a26e899f147aca5cb47e18b533998fefdc
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-02-15 23:41:52 +0330

    router and ref deleted / href fixed

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -2,12 +2,14 @@ import { MdThumbUp } from 'react-icons/md';
 import Image from 'next/image';
 import React, { forwardRef } from 'react';
 import Link from 'next/link';
+import Router from 'next/router';
 
 // motion
 import { motion } from 'framer-motion';
 import { fadeIn } from '../utils';
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+const Thumbnail = forwardRef(({ result }, ref) => { 
+
     const BASE_URL = 'https://image.tmdb.org/t/p/original/';
     return (
         <div className="p-2 group shadow-xl m-2 cursor-pointer overflow-hidden relative transition duration-200 ease-in transform bg-cream dark:bg-[#273d3f] rounded-t-[5px] hover:scale-105 hover:z-50">
@@ -27,7 +29,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
                 initial="hidden"
                 whileInView="show"
                 variants={fadeIn('right', 'tween', 0.2, 1)}
-                ref={ref}
                 className="p-2 space-y-2"
             >
                 <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
@@ -42,7 +43,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
                 </p>
                 <button className="bg-slate-900 hidden hover:-translate-y-1 duration-300 lg:block font-semibold p-2 rounded-md w-full">
                     <Link
-                        href={`/movie-card/${result.id}`}
+                        href="movie-card/[id]"
                         as={`/movie-card/${result.id}`}
                     >
                         more details
