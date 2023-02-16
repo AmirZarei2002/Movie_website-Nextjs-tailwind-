b429274ed31348d20a65efb8a30738d19a81a443
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-02-16 21:00:21 +0330

    motion added

--- pages/movie-card/[id]/index.js
+++ pages/movie-card/[id]/index.js
@@ -5,15 +5,19 @@ import { FiStar } from 'react-icons/fi';
 import { AiOutlineRise, AiOutlineLineChart } from 'react-icons/ai';
 // motion
 import { motion } from 'framer-motion';
-import { fadeIn } from '../../../utils';
+import { fadeIn, slideIn } from '../../../utils';
 
 const MovieCard = ({ card }) => {
-    console.log(card);
     const BASE_URL = 'https://image.tmdb.org/t/p/original/';
     return (
-        <main className="my-[2rem] w-100 h-100 lg:w-[90%] space-y-6 mx-auto">
+        <main className="my-[2rem] w-100 h-100 lg:w-[90%] space-y-6 mx-auto overflow-hidden">
             {/* main details */}
-            <section className="flex justify-between mx-1 text-start space-y-2">
+            <motion.section
+                variants={slideIn('right', 'tween', 0.2, 1)}
+                initial="hidden"
+                whileInView="show"
+                className="flex justify-between mx-1 text-start space-y-2"
+            >
                 <div>
                     <h1 className="font-semibold text-2xl lg:text-3xl xl:text-4xl dark-blue">
                         {card.title || card.original_name}
@@ -51,8 +55,13 @@ const MovieCard = ({ card }) => {
                         </div>
                     </div>
                 </div>
-            </section>
-            <section>
+            </motion.section>
+            {/* movie image */}
+            <motion.section
+                variants={fadeIn('right', 'tween', 0.2, 1)}
+                initial="hidden"
+                whileInView="show"
+            >
                 <Image
                     layout="responsive"
                     height={860}
@@ -64,24 +73,31 @@ const MovieCard = ({ card }) => {
                     }
                     alt="images"
                 />
-            </section>
-            <section className="ml-1 lg:ml-0 space-y-6">
+            </motion.section>
+            {/* complete details */}
+            <motion.section
+                variants={slideIn('left', 'tween', 0.2, 1)}
+                initial="hidden"
+                whileInView="show"
+                className="ml-1 lg:ml-0 space-y-6"
+            >
                 <div className="text-white flex space-x-3">
                     {card.genres.map((genre) => (
-                        <h2 className="border rounded-xl text-slate-900 dark:text-white py-[2px] px-2">
-                            {genre.name}
-                        </h2>
+                        <div key={genre.id}>
+                            <h2 className="border rounded-xl text-slate-900 dark:text-white py-[2px] px-2">
+                                {genre.name}
+                            </h2>
+                        </div>
                     ))}
                 </div>
                 <div className="w-[90%] space-y-2 ml-2 sm:ml-0 text-white">
-                    <h2 className="text-lg sky md:text-xl font-semibold">
-                        "{card.tagline}"
-                    </h2>
-                    <p className="text-justify md:text-lg">{card.overview}</p>
+                    <p className="text-justify text-slate-900 dark:text-white md:text-lg">
+                        {card.overview}
+                    </p>
                 </div>
                 <div className="flex md:hidden space-x-4">
                     <div className="flex flex-col items-center">
-                        <h3 className="capitalize font-semibold text-sm text-slate-900 dark:text-purple-700">
+                        <h3 className="capitalize font-semibold text-sm dark-blue dark:text-purple-700">
                             vote average
                         </h3>
                         <div className="flex items-center space-x-1">
@@ -90,7 +106,7 @@ const MovieCard = ({ card }) => {
                         </div>
                     </div>
                     <div className="flex flex-col items-center">
-                        <h3 className="capitalize font-semibold text-sm text-slate-900 dark:text-purple-700">
+                        <h3 className="capitalize font-semibold text-sm dark-blue dark:text-purple-700">
                             youre rating
                         </h3>
                         <div className="flex items-center space-x-1 dark-blue">
@@ -99,7 +115,7 @@ const MovieCard = ({ card }) => {
                         </div>
                     </div>
                     <div className="flex flex-col items-center">
-                        <h3 className="capitalize font-semibold text-sm text-slate-900 dark:text-purple-700">
+                        <h3 className="capitalize font-semibold text-sm dark-blue dark:text-purple-700">
                             vote count
                         </h3>
                         <div className="flex items-center space-x-1">
@@ -108,7 +124,57 @@ const MovieCard = ({ card }) => {
                         </div>
                     </div>
                 </div>
-            </section>
+                <div className="border border-gray-500" />
+                <div className="flex items-center text-center space-x-2">
+                    <h2 className="text-transform capitalize purple font-semibold">
+                        companies
+                    </h2>
+                    {card.production_companies.map((company) => (
+                        <div key={company.id}>
+                            <p className="dark:cream text-slate-900 dark:text-white font-thin">
+                                {company.name}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+                <div className="border border-gray-500" />
+                <div className="flex items-center text-center space-x-2">
+                    <h2 className="text-transform capitalize purple font-semibold">
+                        countries
+                    </h2>
+                    {card.production_countries.map((country, index) => (
+                        <div key={index}>
+                            <p className="dark:cream text-slate-900 dark:text-white font-thin">
+                                {country.name}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+                <div className="border border-gray-500" />
+                <div className="flex items-center text-center space-x-2">
+                    <h2 className="text-transform capitalize purple font-semibold">
+                        status
+                    </h2>
+                    <p className="dark:cream text-slate-900 dark:text-white font-thin">
+                        {card.status}
+                    </p>
+                </div>
+                <div className="border border-gray-500" />
+                <div className="flex items-center text-center space-x-2">
+                    <h2 className="text-transform capitalize purple font-semibold">
+                        languages
+                    </h2>
+                    {card.spoken_languages.map((language, index) => (
+                        <div key={index}>
+                            <p className="dark:cream text-slate-900 dark:text-white font-thin">
+                                {language.name}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </motion.section>
+            {/* user reviews */}
+            <motion.section></motion.section>
         </main>
     );
 };
@@ -121,6 +187,7 @@ export async function getServerSideProps(context) {
         `${server}/movie/${id}?api_key=${process.env.API_KEY}`
     );
     const card = await res.json();
+
     return {
         props: {
             card,
