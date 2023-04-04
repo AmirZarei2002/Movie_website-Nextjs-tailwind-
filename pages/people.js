a47c6494f95c88cef1a123c72f421d561e4fc9eb
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-04 23:20:13 +0430

    add scrollbar to the pagination

--- pages/people.js
+++ pages/people.js
@@ -56,12 +56,12 @@ export default function Home({ people, totalPages, currentPage }) {
                     </motion.div>
                 ))}
             </div>
-            <div className="flex justify-center mt-8 overflow-hidden">
+            <div className="flex justify-center mt-8 overflow-x-scroll">
                 {Array.from({ length: totalPages }, (_, i) => i + 1).map(
                     (page) => (
                         <Link key={page} href={`?page=${page}`}>
                             <a
-                                className={`bg-cream dark:bg-[#2dcddf] rounded-md px-2 ${
+                                className={`bg-cream dark:bg-[#2dcddf] rounded-md mb-4 px-2 ${
                                     page === currentPage
                                         ? 'font-bold text-[#3c79f5] dark:text-blue-600'
                                         : 'font-semibold text-sky-600 dark:text-slate-700'
