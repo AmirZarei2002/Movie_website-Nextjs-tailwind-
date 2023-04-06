7869093d6784e2dca7b441bfa7267c145a44be89
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-06 20:53:46 +0430

    fix the alignment

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,14 +1,14 @@
 // innder components
-import AccountForm from "../components/account/AccountForm";
-import AccountLogo from "../components/account/AccountLogo";
+import AccountForm from '../components/account/AccountForm';
+import AccountLogo from '../components/account/AccountLogo';
 
 export default function Account() {
     return (
         <main className="relative">
             <div className="container px-6 py-12 h-full">
                 <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-                    <AccountForm />
                     <AccountLogo />
+                    <AccountForm />
                 </div>
             </div>
         </main>
