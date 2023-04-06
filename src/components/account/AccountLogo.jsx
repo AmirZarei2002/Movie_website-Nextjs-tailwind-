f914267b510453c3c09caa6cff63f14d498be8fe
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-06 20:50:39 +0430

    account page devided to two innder components

---
+++ src/components/account/AccountLogo.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+export default function AccountLogo() {
+    return (
+        <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+            <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                className="w-full"
+                alt="Phone image"
+            />
+        </div>
+    );
+}
