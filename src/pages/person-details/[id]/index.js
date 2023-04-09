0f80a3360a4419d458dddb6176a0843e5cb73a5b
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-09 22:55:30 +0430

    create a new simple template for small devices

--- src/pages/person-details/[id]/index.js
+++ src/pages/person-details/[id]/index.js
@@ -1,10 +1,56 @@
+import { useState } from 'react';
+import Image from 'next/image';
 import { server } from '../../../config';
 
 export default function PersonDetail({ person }) {
+    const [showMoreBio, setShowMoreBio] = useState(false);
+    const profileImageUrl = `https://image.tmdb.org/t/p/w500/${person.profile_path}`;
+
+    const shortBio = person.biography.slice(0, 200);
+    const longBio = person.biography;
+
     return (
-       <div>
-        {person.biography}
-       </div>
+        <main className="container mx-auto my-5 space-y-6">
+            {/* personal info */}
+            <section className="text-center flex">
+                <Image
+                    src={profileImageUrl}
+                    width={200}
+                    height={250}
+                    alt={person.name}
+                    className="rounded-r-sm"
+                />
+                <div className="pl-2 text-start flex flex-col space-y-2">
+                    <h1 className="font-bold dark-blue text-2xl">
+                        {person.name}
+                    </h1>
+                    <div className="text-slate-700 flex flex-col">
+                        <span>{person.birthday}</span>
+                        <span>{person.deathday}</span>
+                        <span>{person.place_of_birth}</span>
+                        <span>{person.known_for_department}</span>
+                        <span>{person.popularity}</span>
+                    </div>
+                </div>
+            </section>
+            <section className="text-md px-2 text-slate-700 space-y-2">
+                <h2 className="font-semibold purple text-xl">Biography</h2>
+                <p
+                    className="leading-7 text-justify"
+                    dangerouslySetInnerHTML={{
+                        __html: showMoreBio ? longBio : shortBio,
+                    }}
+                ></p>
+                {person.biography.length > 200 && (
+                    <button
+                        className="text-blue-500 underline"
+                        onClick={() => setShowMoreBio(!showMoreBio)}
+                    >
+                        {showMoreBio ? 'Read Less' : 'Read More'}
+                    </button>
+                )}
+            </section>
+        </main>
     );
 }
 
