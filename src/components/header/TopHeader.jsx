aeed7cea46dbf3d16fa85a22ff901f09b925fbc1
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-10 22:09:41 +0430

    Wrap the logo into a Link headed to the home page Change the size of Website Name in the header

--- src/components/header/TopHeader.jsx
+++ src/components/header/TopHeader.jsx
@@ -1,19 +1,29 @@
 import Image from 'next/image';
 // logo image
-import logoImg from '../../../public/movie-lovers-logo.png'
+import logoImg from '../../../public/movie-lovers-logo.png';
 // dark mode btn
 import DarkModeButton from '../common/DarkModeButton';
 // TypingText
 import { TypingText } from '../common/CustomTexts';
-
+import Link from 'next/link';
 
 export default function TopHeader() {
     return (
         <div className="flex px-2 justify-between items-center md:justify-around lg:justify-between">
-            <Image src={logoImg} alt="movie-lovers-logo" width={55} height={55} className="flex dark:hover:text-cyan-600" />
+            <Link href="/">
+                <a>
+                    <Image
+                        src={logoImg}
+                        alt="movie-lovers-logo"
+                        width={55}
+                        height={55}
+                        className="flex dark:hover:text-cyan-600"
+                    />
+                </a>
+            </Link>
             <TypingText
                 title="Movie Lovers"
-                textStyles="dark:text-cyan-500 dark:animate-pulse italic block md:text-2xl font-semibold pr-7"
+                textStyles="dark:text-cyan-500 italic block text-[23px] md:text-3xl font-semibold pr-7 animate-pulse"
             />
             <DarkModeButton />
         </div>
