da8084ee0d71801e903d0ba2146076368fc67428
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-11 22:59:54 +0430

    Add functionality for hiding death date Add icons for person info

--- src/pages/person-details/[id]/index.js
+++ src/pages/person-details/[id]/index.js
@@ -1,16 +1,43 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
+
+// server
 import { server } from '../../../config';
+
+// icons
+import { MdCake, MdLocationOn } from 'react-icons/md';
+import { BsPersonSquare, BsStarFill } from 'react-icons/bs';
+import { GiHastyGrave } from 'react-icons/gi';
 
 export default function PersonDetail({ person }) {
     const [showMoreBio, setShowMoreBio] = useState(false);
+    const [birthPlace, setBirthPlace] = useState(person.place_of_birth);
     const profileImageUrl = `https://image.tmdb.org/t/p/w500/${person.profile_path}`;
 
     const shortBio = person.biography.slice(0, 200);
     const longBio = person.biography;
 
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 640) {
+                const country = person.place_of_birth.split(', ')[1];
+                setBirthPlace(country);
+            } else {
+                setBirthPlace(person.place_of_birth);
+            }
+        };
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [person.place_of_birth]);
+
     return (
-        <main className="container mx-auto my-5 space-y-6">
+        <main className="container mx-auto my-5 space-y-6 px-2">
             {/* personal info */}
             <section className="text-center flex">
                 <Image
@@ -18,25 +45,44 @@ export default function PersonDetail({ person }) {
                     width={200}
                     height={250}
                     alt={person.name}
-                    className="rounded-r-sm"
+                    className="rounded-t-sm"
                 />
-                <div className="pl-2 text-start flex flex-col space-y-2">
+                </section>
+                <section>
+                <div className="pl-2 text-start flex flex-col space-y-3">
                     <h1 className="font-bold dark-blue text-2xl">
                         {person.name}
                     </h1>
-                    <div className="text-slate-700 flex flex-col">
-                        <span>{person.birthday}</span>
-                        <span>{person.deathday}</span>
-                        <span>{person.place_of_birth}</span>
-                        <span>{person.known_for_department}</span>
-                        <span>{person.popularity}</span>
+                    <div className="text-slate-700 dark:text-[#f2deba] dark:opacity-80 flex flex-col space-y-3">
+                        <div className="flex items-center gap-2">
+                            <MdCake />
+                            <span>{person.birthday}</span>
+                        </div>
+                        {person.deathday && (
+                            <div className="flex items-center gap-2">
+                                <GiHastyGrave />
+                                <span>{person.deathday}</span>
+                            </div>
+                        )}
+                        <div className="flex items-center gap-2">
+                            <MdLocationOn />
+                            <span>{birthPlace}</span>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <BsPersonSquare />
+                            <span>{person.known_for_department}</span>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <BsStarFill />
+                            <span>{person.popularity}</span>
+                        </div>
                     </div>
                 </div>
             </section>
-            <section className="text-md px-2 text-slate-700 space-y-2">
+            <section className="text-md px-2 text-slate-700 space-y-1">
                 <h2 className="font-semibold purple text-xl">Biography</h2>
                 <p
-                    className="leading-7 text-justify"
+                    className="leading-7 text-justify dark:text-white/70"
                     dangerouslySetInnerHTML={{
                         __html: showMoreBio ? longBio : shortBio,
                     }}
