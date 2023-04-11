a49868f5d77da7545b655b58dcf8bd38605029d7
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-12 01:20:09 +0430

    Implement top scrollbar with spring smoothing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,26 @@
 import '../styles/globals.css';
 import MainLayout from '../Layouts/mainLayout';
 
+import { motion, useScroll, useSpring } from "framer-motion";
+
 import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
+    const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
     return (
+        <>
+        <motion.div className="progress-bar" style={{ scaleX }} />
         <ThemeProvider attribute="class">
             <MainLayout>
                 <Component {...pageProps} />
             </MainLayout>
         </ThemeProvider>
+            </>
     );
 }
 
